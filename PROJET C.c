0e6b705c4aa4cb90f34ebb3ee0f1cf2833ec12f9
2004-syrine <syrineabichou@gmail.com>
2025-12-10 22:44:50 +0100

    tache1

--- PROJET C.c
+++ PROJET C.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct {
+    char type[30];
+    float duree;
+    float vitesse;
+    char priorite[10];
+} Commande;
+typedef struct Noeud {
+    Commande cmd;
+    struct Noeud *suivant;
+} Noeud;
+typedef struct {
+    Noeud *tete;
+    Noeud *queue;
+} File;
 void executer_commande(Commande c){
     if(strcmp(c.priorite,URGENTE)==0){
         printf("COMMANDE URGENTE : %s\n",c.type);
